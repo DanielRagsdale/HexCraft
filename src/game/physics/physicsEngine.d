5482ccac99cb3aad0bef630f953b7fc9338095ad
Daniel Ragsdale <contact@DanielRagsdale.com>
2017-09-22 12:00:44 -0700

    Continued testing on the physics system

--- src/game/physics/physicsEngine.d
+++ src/game/physics/physicsEngine.d
@@ -18,27 +18,48 @@ void TickPhysics(ref Map map)
 	{
 		obj.transform.velocity += vec_square(0, -9.8, 0) * PHYSICS_DT;
 		
+		double radius = (cast(IPhysical)obj).GetRadius();
+		
+		vec_square[vec_block] collisions;
+
 		foreach(delta; square_dn)
 		{
-			vec_block blockCoords = obj.transform.position + delta * (cast(IPhysical)obj).GetRadius();
+			vec_block blockCoords = obj.transform.position + delta * radius;
 			
-			vec_square intr = cast(vec_square)blockCoords - obj.transform.position - vec_square(0,1,0);
-
+			if(delta.y == 0)
+				blockCoords = cast(vec_block)(obj.transform.position + delta * radius + vec_square(0, 0.1, 0));
+			
 			if(map.getBlock(blockCoords))
 			{
-				if(map.getBlock(cast(vec_block)(obj.transform.position + delta * (cast(IPhysical)obj).GetRadius() * 0.9)))
-				{
-					vec_square pushOut = delta * (delta*intr) * 0.1 * (cast(IPhysical)obj).GetRadius();
-
-					obj.transform.position -= pushOut;
-				}
-
-				double limitedDot = max (0.0, delta*obj.transform.velocity); 
-				vec_square pComponent = delta * limitedDot;
-				
-				obj.transform.velocity -= pComponent;
+				collisions[blockCoords] = delta;
 			}
 		}
+
+		foreach(blockCoords, delta; collisions)
+		{
+			vec_square intr = cast(vec_square)blockCoords - obj.transform.position + vec_square(0,1,0);
+			
+			double intrSize = delta * intr;
+			double collisionValue = intrSize - radius;
+
+			if(delta.y == 0)
+			{
+				collisionValue = intrSize + 0.5 - radius;
+			}
+
+			if(collisionValue < 0.05 && collisionValue > -0.8)
+			{
+				vec_square pushOut = delta * collisionValue * 0.6;
+
+				obj.transform.position += pushOut;
+			}
+
+			double limitedDot = max (0.0, delta*obj.transform.velocity); 
+			vec_square pComponent = delta * limitedDot;
+			
+			obj.transform.velocity -= pComponent;
+		}
+
 		obj.transform.position += obj.transform.velocity * PHYSICS_DT;
 	}	
 }
